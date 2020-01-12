7c7c9998f7c147d4256d09916452a9253d59ae27
GorgeousMooseNipple <orang768@gmail.com>
2020-01-13 01:23:49 +0300

    Removed login

--- ad_server/__init__.py
+++ ad_server/__init__.py
@@ -2,12 +2,10 @@ from flask import Flask
 from ad_server.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
 
 
 db = SQLAlchemy()
 migrate = Migrate()
-login = LoginManager()
 
 
 def create_app(config=Config):
@@ -15,7 +13,6 @@ def create_app(config=Config):
     app.config.from_object(config)
     db.init_app(app)
     migrate.init_app(app, db)
-    login.init_app(app)
 
     from ad_server.views.users import users as users_bp
     app.register_blueprint(users_bp, url_prefix='/api/public/auth')
